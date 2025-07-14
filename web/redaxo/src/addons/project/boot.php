af12c0ec0bf9773eeea93878f3c53df74e0c4494
shaack <shaack@gmail.com>
2025-07-14 16:57:30 +0200

    added redaxo

---
+++ web/redaxo/src/addons/project/boot.php
@@ -0,0 +1,18 @@
+<?php
+
+$addon = rex_addon::get('project');
+
+// register a custom yrewrite scheme
+// rex_yrewrite::setScheme(new rex_project_rewrite_scheme());
+
+// register yform template path
+// rex_yform::addTemplatePath($addon->getPath('yform-templates'));
+
+// register yorm class
+// rex_yform_manager_dataset::setModelClass('rex_my_table', my_classname::class);
+
+// change list of allowed mime types for mediapool
+// rex_mediapool::setAllowedMimeTypes([
+//     ...rex_mediapool::getAllowedMimeTypes(),
+//     'json' => ['application/json'],
+// ]);
